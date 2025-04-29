4c124a38c723332f4993a8ab618b2eb54e84035c
Joshua Olvera Cruz <jolverac1900@alumno.ipn.mx>
2025-04-28 20:22:46 -0600

    Version 1.1. Logos.

--- app/src/main/java/com/example/appesia/ui/fragments/CreditosFragment.kt
+++ app/src/main/java/com/example/appesia/ui/fragments/CreditosFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.appesia.R
@@ -21,9 +20,6 @@ class CreditosFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        view.findViewById<Button>(R.id.regresarButton).setOnClickListener {
-            findNavController().navigateUp()
-        }
+        requireActivity().title = "Créditos"
     }
 } 
